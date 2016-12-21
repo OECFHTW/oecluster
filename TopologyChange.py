a900cfaa67e9b01ef1a78e024f5da4d58a28fbf5
Susanne <wi13b005@technikum-wien.at>
2016-12-21 21:18:59 +0100

    my first commitment

--- TopologyChange.py
+++ TopologyChange.py
@@ -1,2 +1,17 @@
 #Author Susanne&Stephan&Roman
 #new comment added
+
+
+# template for pinging IP range
+import subprocess
+
+for ping in range(1,255):
+    address = "192.168.1." + str(ping)
+    res = subprocess.call(['ping', '-c', '3', address])
+    if res == 0:
+        print "ping to", address, "OK"
+    elif res == 2:
+        print "no response from", address
+    else:
+        print "ping to", address, "failed!"
+
