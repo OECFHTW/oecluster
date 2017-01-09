624c7b59595eab0675885992e8cb651b70bf2234
SidCorey <matthias.rosskopf@icloud.com>
2017-01-09 20:48:16 +0100

    Update MasterElection.py

--- MasterElection.py
+++ MasterElection.py
@@ -1,15 +1,13 @@
-
+from socket import inet_aton
+import struct
 
 class MasterElection:
 
     def __init__(self):
         print("Init MasterElection")
 
-    def getMaster(self, memberList):
-        print("Elect master for list " + str(memberList))
-        master="127.0.0.1"
-        for clusterMember in memberList:
-            #TODO implement condition
-            master=clusterMember
+    def getMaster(self, *memberList):
+        memberListSorted = sorted(memberList, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
+        master = memberListSorted[0]
         print("Elected Master is " + master)
         return master
